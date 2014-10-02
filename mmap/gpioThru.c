83c7e9a508855aeae3da53e0a920ad75b11cd31f
MarkMorrison <morrisma@rose-hulman.edu>
2014-10-01 21:53:54 -0400

    mmap working

--- mmap/gpioThru.c
+++ mmap/gpioThru.c
@@ -11,6 +11,10 @@
 #include <signal.h>    // Defines signal-handling functions (i.e. trap Ctrl-C)
 #include "beaglebone_gpio.h"
 
+
+#define SYSFS_GPIO_DIR "/sys/class/gpio"
+#define MAX_BUF 64
+
 /****************************************************************
  * Global variables
  ****************************************************************/
@@ -27,12 +31,39 @@ void signal_handler(int sig)
 	keepgoing = 0;
 }
 
+/****************************************************************
+ * gpio_set_dir
+ ****************************************************************/
+int gpio_set_dir(unsigned int gpio, const char* dir)
+{
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	len = snprintf(buf, sizeof(buf), SYSFS_GPIO_DIR  "/gpio%d/direction", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/direction");
+		return fd;
+	}
+
+	write(fd, dir, sizeof(dir)+1);
+
+	close(fd);
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
     volatile void *gpio_addr;
     volatile unsigned int *gpio_oe_addr;
     volatile unsigned int *gpio_datain;
     volatile unsigned int *gpio_setdataout_addr;
     volatile unsigned int *gpio_cleardataout_addr;
+    volatile void *gpio3_addr;
+    volatile unsigned int *gpio3_oe_addr;
+    volatile unsigned int *gpio3_datain;
+    volatile unsigned int *gpio3_setdataout_addr;
+    volatile unsigned int *gpio3_cleardataout_addr;
     unsigned int reg;
 
     // Set the signal callback for Ctrl-C
@@ -45,14 +76,27 @@ int main(int argc, char *argv[]) {
 
     gpio_addr = mmap(0, GPIO0_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 
                         GPIO0_START_ADDR);
+    gpio3_addr = mmap(0, GPIO3_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 
+                        GPIO3_START_ADDR);
 
     gpio_oe_addr           = gpio_addr + GPIO_OE;
     gpio_datain            = gpio_addr + GPIO_DATAIN;
     gpio_setdataout_addr   = gpio_addr + GPIO_SETDATAOUT;
     gpio_cleardataout_addr = gpio_addr + GPIO_CLEARDATAOUT;
+    
+    gpio3_oe_addr           = gpio3_addr + GPIO_OE;
+    gpio3_datain            = gpio3_addr + GPIO_DATAIN;
+    gpio3_setdataout_addr   = gpio3_addr + GPIO_SETDATAOUT;
+    gpio3_cleardataout_addr = gpio3_addr + GPIO_CLEARDATAOUT;
+    
+    gpio_set_dir(4, "out");
 
     if(gpio_addr == MAP_FAILED) {
         printf("Unable to map GPIO\n");
+        exit(1);
+    }
+    if(gpio3_addr == MAP_FAILED) {
+        printf("unable to map GPIO3\n");
         exit(1);
     }
     printf("GPIO mapped to %p\n", gpio_addr);
@@ -61,16 +105,26 @@ int main(int argc, char *argv[]) {
     printf("GPIO CLEARDATAOUT mapped to %p\n", gpio_cleardataout_addr);
 
     printf("Start copying GPIO_07 to GPIO_03\n");
+    printf("Start copying GPIO_115 to GPIO_30 and GPIO_31\n");
+    
     while(keepgoing) {
     	if(*gpio_datain & GPIO_07) {
             *gpio_setdataout_addr= GPIO_03;
     	} else {
             *gpio_cleardataout_addr = GPIO_03;
     	}
+    	if(*gpio3_datain & GPIO_19) {
+    	    *gpio_setdataout_addr= GPIO_30;
+    	    *gpio_setdataout_addr= GPIO_31;
+    	} else {
+    	    *gpio_cleardataout_addr = GPIO_30;
+    	    *gpio_cleardataout_addr = GPIO_31;
+    	}
         usleep(0);
     }
 
     munmap((void *)gpio_addr, GPIO0_SIZE);
+    munmap((void *)gpio3_addr, GPIO3_SIZE);
     close(fd);
     return 0;
 }
